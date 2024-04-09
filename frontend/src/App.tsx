c5ef2945d7973478ea1c351981c22c9d4c88507c
Fanny Shehabi <fanny.shehabi@outlook.fr>
2024-04-09 16:43:07 +0200

    frontend: navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Map from './components/Map';
 import DetailsAccident from './components/DetailsAccident';
 import './App.css';
@@ -8,10 +8,24 @@ import Signalement from './components/Signalement';
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Router>
       <header className="App-header">
         <h1>Sécurité des Cyclistes à Rennes</h1>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/" className="links">Accueil</Link>
+            </li>
+            <li className="links">
+              Carte (à link)
+            </li>
+            <li>
+              <Link to="/signalement" className="signaler-button">Signaler</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
-      <Router>
+      
       <Routes>
           <Route path="/accidents/:id" element={<DetailsAccident />} />
           <Route path="/signalement" element={<Signalement />} />
